c6ed3a92d966c7155dde9b93a38ea0fbc67a3b8b
Gilvan Gomes <gilvango.j@gmail.com>
2022-04-16 18:56:40 -0300

    feat(header): creation of responsive header structure
    creation of responsive header structure, with icon for menu expansion on devices of limited screen
    size. (Continued, the last commit was incomplete)

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Flex, useBreakpointValue, IconButton, Icon, Text,
+} from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+
+import { useSidebarDrawer } from '../../contexts/SidebarDrawer';
+
+export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  return (
+    <Flex
+      as="header"
+      align="center"
+      mx="auto"
+      mt="4"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+    >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          color="green.300"
+          icon={<Icon as={RiMenuLine} />}
+          mr="2"
+          fontSize="3xl"
+          variant="unstyled"
+          onClick={onOpen}
+        />
+      )}
+      <Flex
+        align="center"
+        mx="auto"
+        justifyContent="center"
+        w="100%"
+        maxWidth={1480}
+        h="20"
+      >
+        <Text
+          fontSize={['2xl', '4xl']}
+          fontWeight="bold"
+          color="green.900"
+          w="128"
+        >
+          VACINASI
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
