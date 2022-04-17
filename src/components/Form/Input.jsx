09256def0f2e1a9fc9d6d4b5ffd0c48348c2a6e1
Gilvan Gomes <gilvango.j@gmail.com>
2022-04-17 13:46:11 -0300

    feat: creation of input component for forms

---
+++ src/components/Form/Input.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React, { forwardRef } from 'react';
+import {
+  Input as ChakraInput,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+
+function InputBase({
+  name, label, error = null, ...rest
+}, ref) {
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        variant="filled"
+        size="lg"
+        focusBorderColor="green.500"
+        bgColor="gray.900"
+        _hover={{
+          bgColor: 'gray.900',
+        }}
+        ref={ref}
+        {...rest}
+      />
+
+      {!!error && (
+        <FormErrorMessage>
+          {error.message}
+        </FormErrorMessage>
+      )}
+    </FormControl>
+  );
+}
+
+export const Input = forwardRef(InputBase);
